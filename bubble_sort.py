53888ac2e8f1bb9d67ea13345c6605b1e0c33af6
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2020-06-14 00:27:44 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,7 +3,7 @@ n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 swaps=0
 for i in range(0,(len(a)-1)):
-    for j in range(0,len(a)-1-i):
+    for j in range(0,len(a)-1-i): #here the elements are being sorted at the end of the array,i.e from the last element
         if a[j]>a[j+1]:
             a[j], a[j+1]=a[j+1], a[j]
             swaps +=1
