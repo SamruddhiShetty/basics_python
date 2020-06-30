c494fb7b956035c799b2a1cfd7d328b722b9f8e1
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2020-06-30 13:42:17 +0530

    Create insert_node_doubly_linked_list.py

---
+++ insert_node_doubly_linked_list.py
@@ -0,0 +1,82 @@
+# basics_python
+
+#insert the node in such a way that the link remains sorted
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+def sortedInsert(head, data):
+    n=DoublyLinkedListNode(data)
+    if head.data>n.data:
+        n.next=head
+        head.prev=n
+        return n
+    current=head
+    while current.data <= n.data and current.next!=None:
+        current=current.next
+    if current.next==None and current.data<=n.data:
+        n.prev=current
+        current.next=n
+        return head
+    n.prev=current.prev
+    n.next=current
+    current.prev.next=n
+    current.prev=n
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
