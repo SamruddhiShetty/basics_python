cdeb316de95470dbba026e7edaaa1c36bcfe5f0e
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2020-07-02 15:59:44 +0530

    Create level_order_traversal_tree.py

---
+++ level_order_traversal_tree.py
@@ -0,0 +1,63 @@
+# basics_python
+#level order traversal also same as breadth first traversal
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def levelOrder(root):
+    #Write your code here
+    q=[]
+    q.append(root)
+    while q:
+        node=q.pop(0)
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+        print(node.info, end=" ")
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
