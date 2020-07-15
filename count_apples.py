3bddb052fdc073e15e87449486bebb5b3a9c7994
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2020-07-15 11:27:04 +0530

    Create count_apples.py

---
+++ count_apples.py
@@ -0,0 +1,40 @@
+# basics_python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+#written to understand diff way of list comprehension
+    print(sum([1 for i in apples if a+i>=s and a+i<=t]))
+    print(sum([1 for i in oranges if b+i<=t and b+i>=s ]))
+
+    
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
