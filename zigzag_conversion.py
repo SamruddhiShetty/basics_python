5898e775e33d519dc526fd943b84a6116b3b0a3a
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2020-07-15 12:43:31 +0530

    Create zigzag_conversion.py

---
+++ zigzag_conversion.py
@@ -0,0 +1,25 @@
+# basics_python
+
+#using visit by row 
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1:
+            return s
+        outlist=[""]*numRows
+        k=0
+        dummy=1
+        for i in s:
+            outlist[k] +=i
+            k +=dummy
+            if k==0 or k==numRows-1:
+                dummy *=-1
+        s1=""
+        for i in outlist:
+            s1 +=i
+            
+        return s1
+ #efficiency is time=O(n) and space=O(n).
+ 
+ #also be done by sort by row
+            
+        
